bbaf416c058c60e3f290f0f96248624379855283
betulemiroglu <33221515+betulemiroglu@users.noreply.github.com>
2018-03-20 13:53:30 +0300

    Update satislistele.php
    oldu  mu bu kez ?

--- satislistele.php
+++ satislistele.php
@@ -13,7 +13,16 @@
 		$kaynak=fgets($dosya,256);
 		$kaynak=trim($kaynak);
 		$dizi=explode("-",$kaynak);
-		
+	<table border="1" align="center">
+<th>ANA MENU</th>
+<tr>
+	<td>
+		<a href='ogrencibilgisi.php'>Ogrenci Bilgisi Gir</a><br>
+		<a href='notbilgisi.php'>Not Bilgisi Gir</a><br>
+		<a href='bilgilistele.php'>Bilgileri Listele</a>
+	</td>
+</tr>
+</table>	
 		
 		
 		
